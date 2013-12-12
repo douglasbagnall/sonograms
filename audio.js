4dac2cd78ac9ac8b1dc5e8a6a54f21427090c858
Douglas Bagnall <douglas@halo.gen.nz>
2013-12-13 12:37:42 +1300

    Multi-row spectrogram with playback.
    Some things are a bit askew -- for instance the playbar misses the
    spectrogram.
    
    The positioning of drawn classifications is not being calculated.

--- audio.js
+++ audio.js
@@ -1,6 +1,6 @@
 var wav_1_minute = 'RFPT-WW13-20111229213002-540-60-KR8.wav';
 var wav_15_minute = "RFPT-WW10A-2013-02-14T02.00.10-KR5.wav";
-var URL = wav_1_minute;
+var URL = wav_15_minute;
 
 var COLOUR_LUT = {
     k: "#0ff",
@@ -51,54 +51,76 @@ function parse_wav(raw){
     };
 }
 
-
-function fill_canvas(audio, samplerate, native_audio){
-    var canvas = document.getElementById('fft');
-    var context = canvas.getContext('2d');
-    var width = canvas.width;
-    var spacing = audio.length / width;
-    var pixel2sec = native_audio.duration / width;
-    var audio_source;
-    var window_size = 1024;
-    var fft_downsample = 2;
-
-    var fft = new FFT(window_size, samplerate);
-    var imgdata = context.createImageData(canvas.width, canvas.height);
-    var pixels = imgdata.data;
+function hann_window(length){
     var i;
-    var left, col;
-    var mask_window = new Float32Array(window_size);
-    var data_window = new Float32Array(window_size);
-    var tau_norm = Math.PI * 2 / window_size;
-    for (i = 0; i < window_size; i++){
-        mask_window[i] = 0.5 - 0.5 * Math.cos(tau_norm * i);
+    var window = new Float32Array(length);
+    var tau_norm = Math.PI * 2 / length;
+    for (i = 0; i < length; i++){
+        window[i] = 0.5 - 0.5 * Math.cos(tau_norm * i);
     }
+    return window;
+}
 
-    var window_floor = 200;
-    var spectrum_low = 10;
-    var spectrum_high = 210;
-    for (left = 0, col = 0; left + window_size < audio.length; left += spacing, col++){
+function paint_spectrogram(audio, window_size, samplerate, pixels, width, height){
+    var i;
+    var left, col, row;
+    var fft = new FFT(window_size, samplerate);
+    var spacing = 120 * samplerate / (width); /* fit 2 minutes across */
+    var mask_window = hann_window(window_size);
+    var data_window = new Float32Array(window_size);
+    var window_floor = 180;
+    var window_spacing = 200;
+    var spectrum_low = 20;
+    var spectrum_high = 200;
+    var pixwidth = width * 4;
+    for (left = 0, col = 0, row = 0;
+         left + window_size <= audio.length;
+         left += spacing, col++){
         var square_window = audio.subarray(left, left + window_size);
         for (i = 0; i < window_size; i++){
             data_window[i] = square_window[i] * mask_window[i];
         }
         fft.forward(data_window);
         var s = fft.spectrum; /*power spectrum, normalised somehow*/
+        if (col >= width){
+            col -= width;
+            row++;
+            console.log(col, row);
+        }
+        var base_offset = ((row * window_spacing + window_floor) * width + col) * 4;
         for (i = spectrum_low; i < spectrum_high; i++){
-            var o = ((window_floor + spectrum_low - i) * width + col) * 4;
+            var o = base_offset - i * pixwidth;
             var v = s[i * 2] + s[i * 2 + 1];
             //console.log(i, o, v);
-            pixels[o] = v * v * 7e8;
+            pixels[o] = v * v * 6e8;
             pixels[o + 1] = v * 3e5;// + Math.sin(v * 1e3) * 50;
             pixels[o + 2] = Math.sqrt(v) * 6e3;
             pixels[o + 3] = 255;
         }
     }
+}
+
+function fill_canvas(audio, samplerate, native_audio){
+    var canvas = document.getElementById('fft');
+    var context = canvas.getContext('2d');
+    var width = canvas.width;
+    var pixel2sec = 120 / width;
+    var row_height = 200;
+    var height = Math.ceil(audio.length / samplerate / 120) * row_height;
+    canvas.height = height;
+    var audio_source;
+    var window_size = 1024;
+    var imgdata = context.createImageData(canvas.width, canvas.height);
+    var pixels = imgdata.data;
+
+    paint_spectrogram(audio, window_size, samplerate,
+                      pixels, canvas.width, canvas.height);
     context.putImageData(imgdata, 0, 0);
+
     function refill_background(){
         var data = context.getImageData(0, 0, canvas.width, canvas.height);
         var pix = data.data;
-        for (i = 0; i < 4 * canvas.width * canvas.height; i += 4){
+        for (var i = 0; i < pix.length; i += 4){
             if (pix[i] == 0 && pix[i + 1] == 0 && pix[i + 2] == 0){
                 pix[i] = pixels[i];
                 pix[i + 1] = pixels[i + 1];
@@ -109,6 +131,7 @@ function fill_canvas(audio, samplerate, native_audio){
     }
     var drawing = 0;
     var playing_column = -1;
+    var playing_row = -1;
     var hidden_data;
     var playing_column_interval;
     var colour = COLOUR_LUT['m'];
@@ -134,13 +157,15 @@ function fill_canvas(audio, samplerate, native_audio){
         }(c);
     }
     function advance_playing_line(){
-        context.putImageData(hidden_data, playing_column, 0);
+        context.putImageData(hidden_data, playing_column, playing_row * row_height);
         playing_column++;
         if (playing_column >= canvas.width){
             playing_column = 0;
+            playing_row++;
         }
-        hidden_data = context.getImageData(playing_column, 0, 1, canvas.height);
-        context.fillRect(playing_column, 0, 1, canvas.height);
+        hidden_data = context.getImageData(playing_column, playing_row * row_height,
+                                           1, row_height);
+        context.fillRect(playing_column, playing_row * row_height, 1, row_height);
     }
     function stop_playing(){
         if (audio_source !== undefined){
@@ -149,19 +174,21 @@ function fill_canvas(audio, samplerate, native_audio){
         audio_source = undefined;
     }
 
-    function start_playing_at_column(x){
+    function start_playing_at_point(x, y){
         stop_playing();
         audio_source = audio_context.createBufferSource();
         audio_source.buffer = native_audio;
         audio_source.connect(audio_context.destination);
-        audio_source.start(0, x * pixel2sec);
+        var row = parseInt(y / row_height);
+        audio_source.start(0, x * pixel2sec + row * 120);
         if (hidden_data !== undefined && playing_column !== undefined){
-            context.putImageData(hidden_data, playing_column, 0);
+            context.putImageData(hidden_data, playing_column, playing_row * row_height);
         }
         playing_column = x;
+        playing_row = row;
         context.fillStyle = "#ff3";
-        hidden_data = context.getImageData(x, 0, 1, canvas.height);
-        context.fillRect(x, 0, 1, canvas.height);
+        hidden_data = context.getImageData(x, row * row_height, 1, row_height);
+        context.fillRect(x, row * row_height, 1, row_height);
         playing_column_interval = window.setInterval(advance_playing_line,
                                                      pixel2sec * 1000);
         audio_source.onended = function(id){
@@ -174,7 +201,8 @@ function fill_canvas(audio, samplerate, native_audio){
     canvas.onclick = function(e){
         if (e.shiftKey){
             var x = e.pageX - this.offsetLeft;
-            start_playing_at_column(x);
+            var y = e.pageY - this.offsetTop;
+            start_playing_at_point(x, y);
         }
     };
 
@@ -211,7 +239,7 @@ function fill_canvas(audio, samplerate, native_audio){
         }
         else if (c == ' '){
             if (audio_source === undefined){
-                start_playing_at_column(playing_column);
+                start_playing_at_point(playing_column, playing_row);
             }
             else {
                 stop_playing();
